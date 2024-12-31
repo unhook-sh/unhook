f8f99cccb5798a934968198754e3f2301d8915df
Chris Watts <chris.watts.t@gmail.com>
2024-12-30 19:14:56 -0800

    fix clerk

--- apps/web-app/src/app/api/webhooks/clerk/route.ts
+++ apps/web-app/src/app/api/webhooks/clerk/route.ts
@@ -1,33 +1,41 @@
 import type { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
+import { eq } from "drizzle-orm";
+import { PostHog } from "posthog-node";
 import { Webhook } from "svix";
 
 import { db } from "@acme/db/client";
-import { Users } from "@acme/db/schema";
+import { ShelterMembers, Shelters, Users } from "@acme/db/schema";
 
-import { env } from "~/env";
+import { env } from "~/env.server";
 
 export const runtime = "edge";
 
 export async function POST(request: Request) {
+  const posthog = new PostHog(env.POSTHOG_KEY, {
+    flushAt: 1,
+    flushInterval: 0,
+    host: "https://us.i.posthog.com",
+  });
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = env.WEBHOOK_SECRET;
+  const CLERK_WEBHOOK_SECRET = env.CLERK_WEBHOOK_SECRET;
 
-  if (!WEBHOOK_SECRET) {
-    throw new Error(
+  if (!CLERK_WEBHOOK_SECRET) {
+    return new Response(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
+      { status: 400 },
     );
   }
 
   // Get the headers
   const headerPayload = await headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  const svixId = headerPayload.get("svix-id");
+  const svixTimestamp = headerPayload.get("svix-timestamp");
+  const svixSignature = headerPayload.get("svix-signature");
 
   // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response("Error occurred -- no svix headers", {
       status: 400,
     });
   }
@@ -38,43 +46,160 @@ export async function POST(request: Request) {
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(CLERK_WEBHOOK_SECRET);
 
   let event: WebhookEvent;
 
   // Verify the payload with the headers
   try {
     event = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-signature": svix_signature,
-      "svix-timestamp": svix_timestamp,
+      "svix-id": svixId,
+      "svix-signature": svixSignature,
+      "svix-timestamp": svixTimestamp,
     }) as WebhookEvent;
   } catch (error) {
     console.error("Error verifying webhook:", error);
-    return new Response("Error occured", {
+    return new Response("Error occurred", {
       status: 400,
     });
   }
 
   // Do something with the payload
-  // For this guide, you simply log the payload to the console
   const { id } = event.data;
   const eventType = event.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  // console.log("Webhook body:", body);
 
   if (event.type === "user.created") {
-    await db.insert(Users).values({
-      avatarUrl: event.data.image_url,
-      email:
-        event.data.email_addresses.find(
-          (email) => email.id === event.data.primary_email_address_id,
-        )?.email_address ?? "",
-      firstName: event.data.first_name,
-      id: event.data.id,
-      lastName: event.data.last_name,
+    const email = event.data.email_addresses.find(
+      (email) => email.id === event.data.primary_email_address_id,
+    )?.email_address;
+
+    if (!email) {
+      return new Response(
+        "Email not found on user.created " + JSON.stringify(event.data),
+        { status: 400 },
+      );
+    }
+
+    const [user] = await db
+      .insert(Users)
+      .values({
+        avatarUrl: event.data.image_url,
+        clerkId: event.data.id,
+        email,
+        firstName: event.data.first_name,
+        id: event.data.id,
+        lastName: event.data.last_name,
+      })
+      .onConflictDoUpdate({
+        set: {
+          avatarUrl: event.data.image_url,
+          clerkId: event.data.id,
+          email,
+          firstName: event.data.first_name,
+          lastName: event.data.last_name,
+        },
+        target: Users.email,
+      })
+      .returning({
+        id: Users.id,
+      });
+
+    if (!user) {
+      return new Response("User not found on user.created", { status: 400 });
+    }
+
+    // Find first shelter and create shelter member
+    const [shelter] = await db.select().from(Shelters).limit(1);
+
+    if (!shelter) {
+      return new Response("No shelter found", { status: 400 });
+    }
+
+    await db.insert(ShelterMembers).values({
+      createdByUserId: user.id,
+      role: "user",
+      shelterId: shelter.id,
+      userId: user.id,
+    });
+
+    posthog.capture({
+      distinctId: user.id,
+      event: "create_user",
+      properties: {
+        email,
+        firstName: event.data.first_name,
+        lastName: event.data.last_name,
+      },
     });
   }
 
+  if (event.type === "user.updated") {
+    const email = event.data.email_addresses.find(
+      (email) => email.id === event.data.primary_email_address_id,
+    )?.email_address;
+
+    const [user] = await db
+      .update(Users)
+      .set({
+        avatarUrl: event.data.image_url,
+        email,
+        firstName: event.data.first_name,
+        lastName: event.data.last_name,
+      })
+      .where(eq(Users.clerkId, event.data.id))
+      .returning({
+        email: Users.email,
+        id: Users.id,
+      });
+
+    if (!user) {
+      return new Response("User not found on user.update", { status: 400 });
+    }
+
+    posthog.capture({
+      distinctId: user.id,
+      event: "update_user",
+      properties: {
+        email: user.email,
+      },
+    });
+  }
+
+  if (event.type === "session.created") {
+    const existingUser = await db.query.Users.findFirst({
+      where: eq(Users.clerkId, event.data.user_id),
+    });
+
+    if (!existingUser) {
+      console.log("User not found on session.created", event.data.user_id);
+      return new Response("", { status: 200 });
+    }
+
+    const [user] = await db
+      .update(Users)
+      .set({
+        lastLoggedInAt: new Date(),
+      })
+      .where(eq(Users.clerkId, event.data.user_id))
+      .returning({
+        email: Users.email,
+        id: Users.id,
+      });
+
+    if (!user) {
+      return new Response("User not found on session.created", { status: 400 });
+    }
+
+    posthog.capture({
+      distinctId: user.id,
+      event: "login",
+      properties: {
+        email: user.email,
+      },
+    });
+  }
+
+  await posthog.shutdown();
   return new Response("", { status: 200 });
 }

d155ab07bc92f6fa36e2ef55f9f0b7fb86928e75
Chris Watts <chris.watts.t@gmail.com>
2025-03-22 09:45:51 -0700

    fix: only import react-scan in dev

--- packages/ui/src/custom/react-scan.tsx
+++ packages/ui/src/custom/react-scan.tsx
@@ -1,20 +1,22 @@
 'use client'
 
-// biome-ignore lint/style/useImportType: react-scan must be imported before react
-import { scan } from 'react-scan'
-
 import { useSearchParams } from 'next/navigation'
 import { type JSX, useEffect } from 'react'
-//
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 export function ReactScan(): JSX.Element {
   const pathParams = useSearchParams()
   const enabled = pathParams.get('react-scan') === 'true'
 
   useEffect(() => {
-    scan({
-      enabled,
-    })
+    if (enabled && isDevelopment) {
+      import('react-scan').then(({ scan }) => {
+        scan({
+          enabled,
+        })
+      })
+    }
   }, [enabled])
 
   return <></>

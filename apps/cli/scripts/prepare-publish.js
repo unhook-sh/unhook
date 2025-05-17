fb8ec84ce97848a29ddc9ace731d675a53c59f9c
Chris Watts <chris.watts.t@gmail.com>
2025-05-16 23:00:59 -0700

    fix: remove workspace deps on publish

--- apps/cli/scripts/prepare-publish.js
+++ apps/cli/scripts/prepare-publish.js
@@ -8,14 +8,20 @@ const packageJsonPath = join(__dirname, '..', 'package.json');
 // Read the package.json
 const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
 
-// Get all @unhook/* dependencies
-const unhookDeps = Object.keys(packageJson.dependencies || {}).filter((dep) =>
-  dep.startsWith('@unhook/'),
-);
+// Get all @unhook/* dependencies from both dependencies and devDependencies
+const unhookDeps = [
+  ...Object.keys(packageJson.dependencies || {}).filter((dep) =>
+    dep.startsWith('@unhook/'),
+  ),
+  ...Object.keys(packageJson.devDependencies || {}).filter((dep) =>
+    dep.startsWith('@unhook/'),
+  ),
+];
 
-// Remove workspace dependencies
+// Remove workspace dependencies from both sections
 for (const dep of unhookDeps) {
-  delete packageJson.dependencies[dep];
+  delete packageJson.dependencies?.[dep];
+  delete packageJson.devDependencies?.[dep];
 }
 
 // Write the modified package.json back
